c9e7ffebd2c6fa72e5676de3207b8c24506788a3
Florian Frank <Florian.Frank@uni-passau.de>
2021-07-07 07:57:40 +0200

    Add name definitions for memory modules to write the memory type inside the meta data file.

--- Inc/memory_defines.h
+++ Inc/memory_defines.h
@@ -8,9 +8,9 @@
 //#define MRAM_EVERSPIN_MR4A08BYS35 1
 //#define MRAM_EVERSPIN_MR4A08BUYS45 1
 //#define FRAM_CYPRESS_FM22L16_55_TG 1
-//#define FRAM_ROHM_MR48V256CTAZAARL 1
+#define FRAM_ROHM_MR48V256CTAZAARL 1
 //#define FRAM_FUJITSU_MB85R1001ANC_GE1 1
-#define RERAM_ADESTO_RM25C512C_LTAI_T 1
+//#define RERAM_ADESTO_RM25C512C_LTAI_T 1
 //#define RERAM_FUJITSU_MB85AS4MTPF_G_BCERE1 1
 
 #define PARALLEL 0
@@ -18,9 +18,10 @@
 
 #ifdef SRAM_ALLIANCE_AS7C34098A
 
+#define MEM_NAME                    SRAM_ALLIANCE_AS7C34098A
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)262144)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT	    16
 #define MEM_ACCESS_TIME_NS			((uint32_t)12)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)3000)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -31,9 +32,10 @@
 
 #ifdef SRAM_ALLIANCE_AS7C38098A
 
+#define MEM_NAME                    SRAM_ALLIANCE_AS7C38098A
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)524288)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT		16
 #define MEM_ACCESS_TIME_NS			((uint32_t)10)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)2700)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -44,9 +46,10 @@
 
 #ifdef SRAM_CYPRESS_CY62157EV30
 
+#define MEM_NAME                    SRAM_CYPRESS_CY62157EV30
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)524288)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT		16
 #define MEM_ACCESS_TIME_NS			((uint32_t)45)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)2200)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -57,9 +60,10 @@
 
 #ifdef SRAM_CYPRESS_CY7C1041CV33
 
+#define MEM_NAME                    SRAM_CYPRESS_CY7C1041CV33
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)262144)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT		16
 #define MEM_ACCESS_TIME_NS			((uint32_t)8)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)3000)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -70,9 +74,10 @@
 
 #ifdef SRAM_CYPRESS_CY7C1051DV33
 
+#define MEM_NAME                    SRAM_CYPRESS_CY7C1051DV33
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)524288)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT		16
 #define MEM_ACCESS_TIME_NS			((uint32_t)10)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)3000)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -83,9 +88,10 @@
 
 #ifdef SRAM_ISSI_IS61WV51216BLL
 
+#define MEM_NAME                    SRAM_ISSI_IS61WV51216BLL
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)524288)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT		16
 #define MEM_ACCESS_TIME_NS			((uint32_t)10)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)2400)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -96,9 +102,10 @@
 
 #ifdef MRAM_EVERSPIN_MR4A08BYS35
 
+#define MEM_NAME                    MRAM_EVERSPIN_MR4A08BYS35
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)2097152)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)8)
+#define MEM_ACCESS_WIDTH_BIT		8
 #define MEM_ACCESS_TIME_NS			((uint32_t)35)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)3000)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -109,9 +116,10 @@
 
 #ifdef MRAM_EVERSPIN_MR4A08BUYS45
 
+#define MEM_NAME                    MRAM_EVERSPIN_MR4A08BUYS45
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)2097152)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)8)
+#define MEM_ACCESS_WIDTH_BIT		8
 #define MEM_ACCESS_TIME_NS			((uint32_t)45)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)3000)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -122,9 +130,10 @@
 
 #ifdef FRAM_CYPRESS_FM22L16_55_TG
 
+#define MEM_NAME                    FRAM_CYPRESS_FM22L16_55_TG
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)262144)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)16)
+#define MEM_ACCESS_WIDTH_BIT		16
 #define MEM_ACCESS_TIME_NS			((uint32_t)55)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)2700)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -135,9 +144,10 @@
 
 #ifdef FRAM_ROHM_MR48V256CTAZAARL
 
+#define MEM_NAME                    FRAM_ROHM_MR48V256CTAZAARL
 #define MEM_ACCESS_IF 				PARALLEL
-#define MEM_SIZE_ADR 				((uint32_t)32768)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)8)
+#define MEM_SIZE_ADR 				32768
+#define MEM_ACCESS_WIDTH_BIT		8
 #define MEM_ACCESS_TIME_NS			((uint32_t)70)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)2700)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -148,9 +158,10 @@
 
 #ifdef FRAM_FUJITSU_MB85R1001ANC_GE1
 
+#define MEM_NAME                    FRAM_FUJITSU_MB85R1001ANC_GE1
 #define MEM_ACCESS_IF 				PARALLEL
 #define MEM_SIZE_ADR 				((uint32_t)131072)
-#define MEM_ACCESS_WIDTH_BIT		((uint8_t)8)
+#define MEM_ACCESS_WIDTH_BIT		8
 #define MEM_ACCESS_TIME_NS			((uint32_t)150)
 #define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)3000)
 #define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
@@ -161,14 +172,15 @@
 
 #ifdef RERAM_ADESTO_RM25C512C_LTAI_T
 
+#define MEM_NAME                    RERAM_ADESTO_RM25C512C_LTAI_T
 #define MEM_ACCESS_IF 				SPI
-#define MEM_SIZE_ADR 				512
-#define MEM_ACCESS_WIDTH_BIT		8
-#define MEM_ACCESS_TIME_NS			150
-#define MEM_SUPPLY_VOLTAGE_MIN_MV	1650
-#define MEM_SUPPLY_VOLTAGE_MAX_MV	3600
-#define MEM_OP_TEMP_MAX_K			233
-#define MEM_OP_TEMP_MIN_K			358
+#define MEM_SIZE_ADR 				((uint32_t)512)
+#define MEM_ACCESS_WIDTH_BIT		16
+#define MEM_ACCESS_TIME_NS			((uint32_t)150)
+#define MEM_SUPPLY_VOLTAGE_MIN_MV	((uint16_t)1650)
+#define MEM_SUPPLY_VOLTAGE_MAX_MV	((uint16_t)3600)
+#define MEM_OP_TEMP_MAX_K			((uint16_t)233)
+#define MEM_OP_TEMP_MIN_K			((uint16_t)358)
 
 #endif
 
