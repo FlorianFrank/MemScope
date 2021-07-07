ba03c21cbcd8afdec5745c1d606fa2e4ad86a138
Florian Frank <Florian.Frank@uni-passau.de>
2021-07-07 08:54:53 +0200

    Declare enum types as unused to avoid warnings.

--- Inc/meta_data_handler.h
+++ Inc/meta_data_handler.h
@@ -1,3 +1,4 @@
+#include <argz.h>
 //
 // Created by florianfrank on 01.07.21.
 //
@@ -14,12 +15,11 @@ enum {
     Write_Latency,
     Read_Latency,
     Row_Hammering,
-    Supply_Volage_Variation,
-    Temperature_Dependency,
-    Startup_Values
+    Supply_Volage_Variation __unused,
+    Temperature_Dependency __unused,
+    Startup_Values __unused
 } typedef TestType;
 
-char* testNameStr[];
 
 char buffer[4096];
 
@@ -28,11 +28,11 @@ char* CreateMetaData(uint32_t testID, TestType testType, uint32_t memID, uint8_t
                      uint32_t rowOffset, uint32_t columnOffset, uint32_t parallelReadBytes, uint32_t parallelWriteBytes,
                      float voltage, float temperature);
 
-char* CreateMetaDataWriteLatency(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t writeParallel, float voltage, float temperature);
+__unused char* CreateMetaDataWriteLatency(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t writeParallel, float voltage, float temperature);
 
 char* CreateMetaDataReadLatency(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t readCycles, uint32_t readParallel, float voltage, float temperature);
 
-char* CreateMetaDataRowHammering(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t rowOffset, uint32_t columnOffset, uint32_t parallelWrite, uint32_t parallelRead, float voltage, float temperature);
+__unused char* CreateMetaDataRowHammering(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t rowOffset, uint32_t columnOffset, uint32_t parallelWrite, uint32_t parallelRead, float voltage, float temperature);
 
 
 #endif //MEMORY_TESTING_FW_META_DATA_HANDLER_H
