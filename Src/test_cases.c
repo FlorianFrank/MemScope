26816c3a29d36e6d93100c5161c4c6df39d8bcdd
Nico Mexis <nico.mexis@kabelmail.de>
2021-10-15 11:59:33 +0200

    Simple Adesto test case

--- Src/test_cases.c
+++ Src/test_cases.c
@@ -12,7 +12,9 @@
 #include "test_cases.h"
 #include "time_measurement.h"
 
-#define NR_TESTS 100
+#define ADDRESSES_TO_TEST MEM_SIZE_ADR
+#define READ_ITERATIONS 10000
+#define MEM_ID 4
 
 int executeReadLatencyTestFRAMRohm()
 {
@@ -153,120 +155,64 @@ int executeMemoryTest()
 #if MEM_ACCESS_IF == SPI
 int executeWIPPollingTestAdestoReRam()
 {
-    srand(time(NULL));
-    uint8_t randomNumber = rand() % 256;
-
-    int startValue = 0x55;
-    printf("Set random number: 0x%x\n", randomNumber);
-
-    for (int cellCtr = 0; cellCtr < MEM_SIZE_ADR; cellCtr++)
-    {
-        MEM_ERROR errCode = SRAM_Write_8b(0x00 + cellCtr, randomNumber);
-        if(errCode != MEM_NO_ERROR)
-            printf("Line: %d MEM_ERROR: %d\n", __LINE__, errCode);
-
-        WIP_Polling(0);
-
-        uint8_t retValue = 0x00;
-        errCode = SRAM_Read_8b(0x00 + cellCtr, &retValue);
-        if(errCode != MEM_NO_ERROR)
-             printf("MEM_ERROR: %d\n", errCode);
-        if (retValue != randomNumber)
-        {
-               printf("Cell: %d Line: %d Incorrect value 0x%x!=0x%x\n", cellCtr, __LINE__, retValue, randomNumber);
-        }
-    }
-
-    for (int cellCtr = 0; cellCtr < MEM_SIZE_ADR; cellCtr++)
-    {
-        MEM_ERROR errCode = SRAM_Write_8b(0x00 + cellCtr, startValue);
-        if(errCode != MEM_NO_ERROR)
-            printf("Line: %d MEM_ERROR: %d\n", __LINE__, errCode);
-    }
-
-    int MEM_ID = 1;
-    int READ_ITERATIONS = 10000;
-    char pollingBuffer[4096];
-
-    memset(pollingBuffer, 0x00, 4096);
-
-
-    USB_MS_Handle *usbHandle = malloc(sizeof(USB_MS_Handle));
+    USB_MS_Handle *usbHandle = calloc(1, sizeof(USB_MS_Handle));
 
     USB_InitConnection(usbHandle);
     USB_ERROR ret;
-    do
-    {
+    do {
         ret = USB_ExecuteStateMachine(usbHandle, 1000);
-        if (ret.m_ErrCode != USB_NO_ERROR)
-        {
+        if (ret.m_ErrCode != USB_NO_ERROR) {
             printf("Line: %d %s", __LINE__, USB_ReturnErrorCodeStr(ret));
         }
     } while (ret.m_ErrCode != USB_NO_ERROR);
 
-    for (int testCtr = 0; testCtr < NR_TESTS; testCtr++)
-    {
+    char *pollingBuffer = calloc(4096, sizeof(char));
 
+    char *measureFileName = calloc(128, sizeof(char));
+    sprintf(measureFileName, "Measurements%d.csv", MEM_ID);
+    char *measureFile = "From;To;Cell;WIP_Polling\n";
+    uint32_t lenMeasureFile = strlen(measureFile);
+    ret = USB_OpenWriteFile(usbHandle, measureFileName, (uint8_t *) measureFile, &lenMeasureFile,
+                            USB_APPEND | USB_WRITE, true);
+    if (ret.m_ErrCode != USB_NO_ERROR)
+        printf("%s", USB_ReturnErrorCodeStr(ret));
 
-        printf("Write Meta data %d\n", testCtr);
-        char *config = CreateMetaDataReadLatency(testCtr, MEM_ID, 0xAA, 0x55, 0, MEM_SIZE_ADR, READ_ITERATIONS, 0,
-                                                 3.3f, 255);
-        uint32_t lenConfig = strlen(config);
-        char configFileName[50];
-        sprintf(configFileName, "Confmem%d.csv", testCtr);
-        ret = USB_OpenWriteFile(usbHandle, configFileName, (uint8_t *) config, &lenConfig, USB_APPEND | USB_WRITE,
-                                true);
-        if (ret.m_ErrCode != USB_NO_ERROR)
-        {
-              printf("Line: %d %s", __LINE__, USB_ReturnErrorCodeStr(ret));
-        }
-        USB_CloseFile(usbHandle);
-        usbHandle->m_Open = true; // ugly hack
+    uint8_t num1 = 0, num2 = 0;
+    do {
+        do {
+            printf("Numbers: 0x%02x -> 0x%02x\n", num1, num2);
+            for (int addrCtr = 0; addrCtr < ADDRESSES_TO_TEST; addrCtr++) {
+                // Write first value
+                SRAM_Write_8b(addrCtr, num1);
 
-        char measureFileName[50];
-        sprintf(measureFileName, "Meamem%d.csv", testCtr);
+                // Wait until the WEL latch turns reset
+                WIP_Polling(0);
 
-        char *measureFile = "Cell;WIP_Polling\n";
-        uint32_t lenMeasureFile = strlen(measureFile);
-        ret = USB_OpenWriteFile(usbHandle, measureFileName, (uint8_t *) measureFile, &lenMeasureFile,
-                                USB_APPEND | USB_WRITE, true);
-         if (ret.m_ErrCode != USB_NO_ERROR)
-            printf("%s", USB_ReturnErrorCodeStr(ret));
+                // Overwrite value
+                SRAM_Write_8b(addrCtr, num2);
 
+                // Measure write latency
+                int write_latency = WIP_Polling(0);
 
+                sprintf(&pollingBuffer[strlen(pollingBuffer)], "%d;%d;%d;%d\n", num1, num2, addrCtr, write_latency);
 
-        for (int addrCtr = 0; addrCtr < MEM_SIZE_ADR; addrCtr++)
-        {
-            SRAM_Write_8b(addrCtr, 0x55);
-
-            // Wait until the WEL latch turns reset
-            WIP_Polling(0);
-
-            // Write "1010101010"
-            SRAM_Write_8b(addrCtr, 0xAA);
-
-            // Measure write latency
-
-            int write_latency = WIP_Polling(0);
-
-            sprintf(&pollingBuffer[strlen(pollingBuffer)], "%d;%d\n", addrCtr, write_latency);
-
-            if (addrCtr % 100 == 0)
-            {
-                uint32_t lenPollingBuff = strlen(pollingBuffer);
-                USB_ERROR usbError = USB_WriteData(usbHandle, (uint8_t *) pollingBuffer, &lenPollingBuff, true);
-                if (usbError.m_ErrCode != USB_NO_ERROR)
+                if (addrCtr % 100 == 0 || addrCtr >= ADDRESSES_TO_TEST - 1) {
+                    uint32_t lenPollingBuff = strlen(pollingBuffer);
+                    USB_ERROR usbError = USB_WriteData(usbHandle, (uint8_t *) pollingBuffer, &lenPollingBuff, true);
+                    if (usbError.m_ErrCode != USB_NO_ERROR)
                     printf("ERROR %d, Iteration %d MSG: %s", __LINE__, addrCtr, USB_ReturnErrorCodeStr(usbError));
-                memset(pollingBuffer, 0x00, 4096 * sizeof(char));
+                    memset(pollingBuffer, 0x00, 4096 * sizeof(char));
+                }
             }
-        }
-        uint32_t lenPollingBuff = strlen(pollingBuffer);
-        USB_ERROR usbError = USB_WriteData(usbHandle, (uint8_t *) pollingBuffer, &lenPollingBuff, true);
-      if(usbError.m_ErrCode != USB_NO_ERROR)
-          printf("ERROR %d, MSG: %s", __LINE__, USB_ReturnErrorCodeStr(usbError));
-        USB_CloseFile(usbHandle);
+        } while (num2++ != 255);
+        USB_ERROR usbError = USB_Flush(usbHandle);
+        if (usbError.m_ErrCode != USB_NO_ERROR)
+            printf("ERROR %d, MSG: %s", __LINE__, USB_ReturnErrorCodeStr(usbError));
+    } while (num1++ != 255);
 
-    }
+    USB_CloseFile(usbHandle);
+    free(measureFileName);
+    free(pollingBuffer);
     return 0;
 }
 #endif // MEM_ACCESS_IF == SPI
