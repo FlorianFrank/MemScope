39dc8f820090771b2643fb54a3d35940d3952e95
Florian Frank <Florian.Frank@uni-passau.de>
2021-12-07 14:08:54 +0100

    Restructure methods in MemoryControllerSPI header file.

--- Inc/cpp/MemoryControllerWrappers/MemoryControllerSPI.h
+++ Inc/cpp/MemoryControllerWrappers/MemoryControllerSPI.h
@@ -12,7 +12,6 @@ class MemoryControllerSPI: public MemoryController
 public:
     explicit MemoryControllerSPI(InterfaceWrappers &interfaceWrapper);
 
-
     enum
     {
         ReRAM_WREN      = (uint8_t) 0x06,       // Set Write Enable Latch
@@ -40,31 +39,29 @@ public:
 
     } typedef MemoryStatusRegister;
 
+    // Basic read and write functions
     MEM_ERROR MemoryWrite8BitWord(uint32_t adr, uint8_t value) override;
-
     MEM_ERROR MemoryRead8BitWord(uint32_t adr, uint8_t *ret) const override;
-
     MEM_ERROR MemoryWrite16BitWord(uint32_t adr, uint16_t value) override;
-
     MEM_ERROR MemoryRead16BitWord(uint32_t adr, uint16_t *value) const override;
 
+    MEM_ERROR WriteStatusRegister(MemoryStatusRegister *statusRegister, uint8_t value);
+    MEM_ERROR ReadStatusRegister(MemoryStatusRegister *statusRegister);
     static MemoryStatusRegister ParseStatusRegister(uint8_t statusRegister);
     static void PrintStatusRegister(MemoryStatusRegister reg);
-    MEM_ERROR WriteStatusRegister(MemoryStatusRegister *statusRegister, uint8_t value);
-    MEM_ERROR SetPowerDown();
+    uint32_t WIP_Polling(uint32_t timeoutCycles);
+
     MEM_ERROR Set_WriteEnableLatch(bool checkRegister);
     MEM_ERROR Reset_WriteEnableLatch();
 
-
-    MEM_ERROR SendSPICommand(SPI_Commands spiCMD, uint8_t *retValue, bool response);
-    MEM_ERROR ReadStatusRegister(MemoryStatusRegister *statusRegister);
-    uint32_t WIP_Polling(uint32_t timeoutCycles);
-    MEM_ERROR EraseChip();
-    MEM_ERROR ResumeFromPowerDown();
+    MEM_ERROR SetPowerDown();
     MEM_ERROR SetUltraDeepPowerDown();
+    MEM_ERROR ResumeFromPowerDown();
     MEM_ERROR SetSleepMode();
 
+    MEM_ERROR EraseChip();
 
+    MEM_ERROR SendSPICommand(SPI_Commands spiCMD, uint8_t *retValue, bool response);
     // Memory specific functions
     static MEM_ERROR CreateWriteMessageReRAMAdesto(uint32_t address, uint8_t value, uint8_t *retValue, uint16_t *retSize);
 
