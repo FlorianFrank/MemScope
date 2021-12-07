2bdf11364a7386b50e5565f698c35f144f00eb5e
Florian Frank <Florian.Frank@uni-passau.de>
2021-12-07 14:01:36 +0100

    Add interface wrappers base class.

--- Inc/cpp/InterfaceWrappers.h
+++ Inc/cpp/InterfaceWrappers.h
@@ -5,10 +5,27 @@
 #ifndef MEMORY_TESTING_FW_INTERFACEWRAPPERS_H
 #define MEMORY_TESTING_FW_INTERFACEWRAPPERS_H
 
+#include <cstdint>
+#include "io_pin_defines.h"
 
 class InterfaceWrappers
 {
+public:
+    bool isTransferCplt() const;
 
+    void resetTransferCplt();
+
+    uint8_t get_space(char *rx_buffer);
+
+protected:
+    void clearBuffer(uint8_t index);
+    char STRING_BUFFER[STRING_BUFFER_SIZE];
+    // receive buffer
+    char Rx_Data[2];
+    char Rx_Buffer[100];
+    uint8_t Rx_Index;
+    // receive buffer
+    bool Transfer_cplt;
 };
 
 
--- Src/cpp/InterfaceWrappers.cpp
+++ Src/cpp/InterfaceWrappers.cpp
@@ -2,4 +2,40 @@
 // Created by florianfrank on 07.12.21.
 //
 
+#include <cstdint>
+#include <cstddef>
+#include <cstring>
 #include "cpp/InterfaceWrappers.h"
+
+/*
+* @brief					clears the rx_buffer starting from address specified in param index
+* @param uint8_t index		index to start from clearing the buffer
+*/
+void InterfaceWrappers::clearBuffer(uint8_t index)
+{
+    for(uint8_t i = index; i < STRING_BUFFER_SIZE; i++){
+        Rx_Buffer[i] = 0;
+    }
+}
+
+bool InterfaceWrappers::isTransferCplt() const
+{
+        return Transfer_cplt;
+}
+
+void InterfaceWrappers::resetTransferCplt()
+{
+    Transfer_cplt = false;
+}
+
+uint8_t InterfaceWrappers::get_space(char *rx_buffer)
+{
+    for (size_t i = 0; i < strlen(rx_buffer); i++)
+    {
+        if (rx_buffer[i] == 32)
+        {
+            return i;
+        }
+    }
+    return (uint8_t) strlen(rx_buffer);
+}
