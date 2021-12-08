66a33da4a63ba953657d5c42066df5f0ad6777fe
Florian Frank <Florian.Frank@uni-passau.de>
2021-12-08 16:43:17 +0100

    Uncomment conflicting function in STM32HelperFunctions.

--- Src/cpp/STMHelperFunctions.cpp
+++ Src/cpp/STMHelperFunctions.cpp
@@ -5,18 +5,18 @@
 #include "cpp/STMHelperFunctions.h"
 
 #if STM32 // TODO
-#include "../../Drivers/CMSIS/Include/core_cm4.h"
+//#include "../../Drivers/CMSIS/Include/core_cm4.h"
 #endif // STM32
 
 /*static*/ MEM_ERROR STMHelperFunctions::Delay(uint32_t milliseconds)
 {
-#if STM32 // TODO
-    /* Initially clear flag */
+/*#if STM32 // TODO
+    *//* Initially clear flag *//*
     (void) SysTick->CTRL;
     while (milliseconds != 0) {
-        /* COUNTFLAG returns 1 if timer counted to 0 since the last flag read */
+        *//* COUNTFLAG returns 1 if timer counted to 0 since the last flag read *//*
         milliseconds -= (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) >> SysTick_CTRL_COUNTFLAG_Pos;
     }
 #endif // STM32
-    return MemoryErrorHandling::MEM_NO_ERROR;
+    return MemoryErrorHandling::MEM_NO_ERROR;*/
 }
