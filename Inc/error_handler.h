19dab1a49b91bdeae8f0fc9837411142bc683188
Florian Frank <Florian.Frank@uni-passau.de>
2021-12-08 16:32:16 +0100

    Add additional c error handler.

---
+++ Inc/error_handler.h
@@ -0,0 +1,10 @@
+//
+// Created by florianfrank on 06.12.21.
+//
+
+#ifndef MEMORY_TESTING_FW_ERROR_HANDLER_H
+#define MEMORY_TESTING_FW_ERROR_HANDLER_H
+
+void Error_Handler(void);
+
+#endif //MEMORY_TESTING_FW_ERROR_HANDLER_H
---
+++ Src/error_handler.c
@@ -0,0 +1,10 @@
+//
+// Created by florianfrank on 06.12.21.
+//
+
+#include "error_handler.h"
+
+void Error_Handler(void)
+{
+
+}
