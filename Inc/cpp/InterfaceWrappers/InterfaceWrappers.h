5f3d0a4a9fba8cd266e222d29b379ba2f15ff762
Florian Frank <Florian.Frank@uni-passau.de>
2021-12-21 14:00:21 +0100

    Remove uncecessary buffer and TransferCplt functions

--- Inc/cpp/InterfaceWrappers/InterfaceWrappers.h
+++ Inc/cpp/InterfaceWrappers/InterfaceWrappers.h
@@ -19,20 +19,10 @@ public:
     virtual MEM_ERROR SendData(uint8_t *data, uint16_t *size, uint32_t timeout) = 0;
     virtual MEM_ERROR ReceiveData(uint8_t *data, uint16_t *size, uint32_t timeout) = 0;
 
-    // Helper functions
-    bool isTransferCplt() const;
-    void resetTransferCplt();
-    uint8_t get_space(char *rx_buffer);
-
 protected:
-    void clearBuffer(uint8_t index);
     char STRING_BUFFER[STRING_BUFFER_SIZE];
     // receive buffer
-    char Rx_Data[2];
-    char Rx_Buffer[100];
-    uint8_t Rx_Index;
-    // receive buffer
-    bool Transfer_cplt;
+
 };
 
 
