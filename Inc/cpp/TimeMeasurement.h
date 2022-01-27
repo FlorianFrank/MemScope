dbf933c7d58ca950cf5b5d988996cb7b15e3a33a
Florian Frank <Florian.Frank@uni-passau.de>
2022-01-27 12:58:22 +0100

    Adjust TimeMeasurements implementation.  - Now member functions instead of static functions are used  - Add Implementation using std::chrono on Linux

--- Inc/cpp/TimeMeasurement.h
+++ Inc/cpp/TimeMeasurement.h
@@ -5,21 +5,31 @@
 #ifndef MEMORY_TESTING_FW_TIMEMEASUREMENT_H
 #define MEMORY_TESTING_FW_TIMEMEASUREMENT_H
 
-#include <cstdint>
+#include <cstdint> // uint32_t
 
 /**
- * @brief This class provides functions to measure the elapsed time using Cortex M4 registers
+ * @brief This class provides functions to measure the elapsed time using Cortex M4 registers or
+ * std::chrono::system_clock on linux systems.
  */
-class TimeMeasurement{
+class TimeMeasurement
+{
 public:
-    static uint32_t ResetAndStartTimer();
-    static uint32_t GetClockCycles();
-    static uint32_t GetElapsedTimeInNS();
-    static void ResetTimer();
-    static void StopTimer();
-    static uint32_t GetTimer();
+
+    void StarTimer();
+    uint32_t StopTimer();
+    uint32_t GetTimer() const;
+    void ResetTimer();
+    uint32_t ResetAndStartTimer();
+
+    uint32_t GetRawCounter() const;
+    uint32_t GetElapsedTimeInNS() const;
+    uint32_t GetElapsedTimeInMS() const;
+
     static uint32_t TransformClockFrequencyToNs(uint32_t value);
+    static uint32_t TransformClockFrequencyToMs(uint32_t value);
+
+private:
+    unsigned long long m_StartTimer;
+    unsigned long long m_StopTimer;
 };
-
-
 #endif //MEMORY_TESTING_FW_TIMEMEASUREMENT_H
--- Src/cpp/TimeMeasurement.cpp
+++ Src/cpp/TimeMeasurement.cpp
@@ -4,66 +4,115 @@
  */
 #include "cpp/TimeMeasurement.h"
 
-#define start_timer()    *((volatile uint32_t*)0xE0001000) = 0x40000001  // Enable CYCCNT register
-#define stop_timer()     *((volatile uint32_t*)0xE0001000) = 0x40000000  // Disable CYCCNT register
-#define get_timer()      *((volatile uint32_t*)0xE0001004)               // Get value from CYCCNT register
-#define reset_timer()    *((volatile uint32_t*)0xE0001004) = 0x00        // Reset counter
 
+#if STM32
 // Clock frequency is used to convert the clock counts into nanoseconds
-#define CLOCK_FREQUENCY		168 // TODO set dynamically
+#define MULTIPLIER		1/168 * 1e9 // TODO set dynamically
+#else
+#include <chrono>
+#define MULTIPLIER		1
+#endif // STM32
 
 /**
- * @brief Resets the previous timer by resetting the counter
- * and restarts the measurements by setting the CYCCNT register
- * @return start timer value TODO is this really necessary?
+ * @brief Resets the previous timer by disabling the previous counters
+ * and restarts the measurements by setting the CYCCNT register on STM32
+ * or initializing m_StartTimer by ns since 01.01.1970 using chrono::system_clock()
+ * @return start timer value.
  */
 uint32_t TimeMeasurement::ResetAndStartTimer()
 {
     ResetTimer();
-    start_timer();
-    return get_timer();
-}
-
-
-void TimeMeasurement::StopTimer()
-{
-    stop_timer();
+    StarTimer();
+    return m_StartTimer;
 }
 
 /**
- * @brief Returns the current timer value.
+ * @brief This function sets the startTimer member variable with a current counter value.
+ * When using linux systems, the attribute is initialized with the ns since 01.01.1970 using chrono::system_clock().
+ * On Stm32 Systems it is initialized with the current clock count.
+ */
+void TimeMeasurement::StarTimer()
+{
+#if STM32
+    // resolution in clock cycles
+    *((volatile uint32_t*)0xE0001000) = 0x40000001  // Enable CYCCNT register
+    m_StartTimer = *((volatile uint32_t*)0xE0001004);
+#else
+    // Resolution in nanoseconds
+    m_StartTimer = std::chrono::system_clock::now().time_since_epoch().count();
+#endif
+}
+
+/**
+ * @brief This method stops the timer and stores the timer value into the m_StopTimer variable.
+ * When using linux systems, the attribute is initialized with the ns since 01.01.1970 using chrono::system_clock().
+ * @return stop timer.
+ */
+uint32_t TimeMeasurement::StopTimer()
+{
+#if STM32
+    *((volatile uint32_t*)0xE0001000) = 0x40000000;  // Disable CYCCNT register
+    m_StopTimer = *((volatile uint32_t*)0xE0001004);
+#else
+    m_StopTimer = std::chrono::system_clock::now().time_since_epoch().count();
+#endif
+    return m_StopTimer;
+}
+
+/**
+ * @brief Returns the current timer value, if the measurement was not stoped.
+ * If the measurement was stopped previously, the difference between the start and stop timer is returned.
  * @return Timer value.
  */
-/*static*/ uint32_t TimeMeasurement::GetTimer()
+uint32_t TimeMeasurement::GetTimer() const
 {
-    return get_timer();
+    if(m_StopTimer != 0)
+        return m_StopTimer - m_StartTimer;
+    else
+#if STM32
+        return *((volatile uint32_t*)0xE0001004); - m_StartTimer;
+#else
+        return std::chrono::system_clock::now().time_since_epoch().count() - m_StartTimer;
+#endif
 }
 
 /**
- * @brief Resets the counter used to count the clock cycles.
+ * @brief Resets the counter used to count the clock cycles. Sets start and stop timer to 0.
  */
-/*static*/ void TimeMeasurement::ResetTimer()
+void TimeMeasurement::ResetTimer()
 {
-    reset_timer();
+#if STM32
+    *((volatile uint32_t*)0xE0001004) = 0x00;        // Reset counter
+#endif // STM32
+    m_StartTimer = 0;
+    m_StartTimer = 0;
 }
 
 /**
- * @brief Resets the counter used to count the clock cycles.
+ * @brief Return the raw timer value of a counter (difference between start and stop timer).
+ * On STM32 boards, the amount of clock cycles is returned on linux systems, the difference in ns is returned.
  */
-/*static*/ uint32_t TimeMeasurement::GetClockCycles()
+uint32_t TimeMeasurement::GetRawCounter() const
 {
-    uint32_t ret = GetTimer();
-    ResetTimer();
-    return ret;
+    return m_StopTimer - m_StartTimer;
 }
 
 /**
- * @brief Returns the elapsed time since calling StartTimer() in nanoseconds.
+ * @brief Returns the elapsed time since calling StartTimer() in nanosecond resolution.
  * @return elapsed time in ns.
  */
-uint32_t TimeMeasurement::GetElapsedTimeInNS()
+uint32_t TimeMeasurement::GetElapsedTimeInNS() const
 {
     return TransformClockFrequencyToNs(GetTimer());
+}
+
+/**
+ * @brief Returns the elapsed time since calling StartTimer() in milliseconds resolution.
+ * @return elapsed time in ms.
+ */
+uint32_t TimeMeasurement::GetElapsedTimeInMS() const
+{
+    return TransformClockFrequencyToMs(GetTimer());
 }
 
 /**
@@ -73,8 +122,16 @@ uint32_t TimeMeasurement::GetElapsedTimeInNS()
  */
 /*static*/ uint32_t TimeMeasurement::TransformClockFrequencyToNs(uint32_t value)
 {
-    return static_cast<uint32_t>((float)value * 1/CLOCK_FREQUENCY);
+        return static_cast<uint32_t>((float)value * MULTIPLIER);
 }
+
+/*static*/ uint32_t TimeMeasurement::TransformClockFrequencyToMs(uint32_t value)
+{
+        return static_cast<uint32_t>(((float)value * MULTIPLIER) / 1e6);
+}
+
+
+
 
 
 
