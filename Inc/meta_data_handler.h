7315217601208f84842df1da212d35abe5d19c98
Florian Frank <Florian.Frank@uni-passau.de>
2022-02-08 12:17:19 +0100

    Remove unused defines from meta_data_handler

--- Inc/meta_data_handler.h
+++ Inc/meta_data_handler.h
@@ -6,7 +6,6 @@
 #ifndef MEMORY_TESTING_FW_META_DATA_HANDLER_H
 #define MEMORY_TESTING_FW_META_DATA_HANDLER_H
 
-#include "memory_defines.h"
 #include <stdint.h> // uint32_t
 #include <string.h> // strlen
 #include <stdio.h> // sprintf
@@ -15,9 +14,9 @@ enum {
     Write_Latency,
     Read_Latency,
     Row_Hammering,
-    Supply_Volage_Variation __unused,
-    Temperature_Dependency __unused,
-    Startup_Values __unused
+    Supply_Volage_Variation,
+    Temperature_Dependency,
+    Startup_Values
 } typedef TestType;
 
 
@@ -27,11 +26,11 @@ char* CreateMetaData(uint32_t testID, TestType testType, uint32_t memID, uint8_t
                      uint32_t rowOffset, uint32_t columnOffset, uint32_t parallelReadBytes, uint32_t parallelWriteBytes,
                      float voltage, float temperature);
 
-__unused char* CreateMetaDataWriteLatency(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t writeParallel, float voltage, float temperature);
+char* CreateMetaDataWriteLatency(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t writeParallel, float voltage, float temperature);
 
 char* CreateMetaDataReadLatency(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t readCycles, uint32_t readParallel, float voltage, float temperature);
 
-__unused char* CreateMetaDataRowHammering(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t rowOffset, uint32_t columnOffset, uint32_t parallelWrite, uint32_t parallelRead, float voltage, float temperature);
+char* CreateMetaDataRowHammering(uint32_t testID, uint32_t memID, uint32_t initValue, uint32_t valueWritten, uint32_t startAddr, uint32_t endAddr, uint32_t writeCycles, uint32_t rowOffset, uint32_t columnOffset, uint32_t parallelWrite, uint32_t parallelRead, float voltage, float temperature);
 
 
 #endif //MEMORY_TESTING_FW_META_DATA_HANDLER_H
