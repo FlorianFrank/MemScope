fa689d0999d6ae1b2692a289f89f67c80c8be760
Florian Frank <Florian.Frank@uni-passau.de>
2022-02-08 12:18:26 +0100

    Refactor STMHelperFunctions.

--- Src/cpp/STMHelperFunctions.cpp
+++ Src/cpp/STMHelperFunctions.cpp
@@ -4,19 +4,19 @@
  */
 #include "cpp/STMHelperFunctions.h"
 
-#if STM32 // TODO
-//#include "../../Drivers/CMSIS/Include/core_cm4.h"
+#if STM32
+#include "stm32f4xx_hal_def.h"
 #endif // STM32
 
 /*static*/ MEM_ERROR STMHelperFunctions::Delay(uint32_t milliseconds)
 {
-/*#if STM32 // TODO
-    *//* Initially clear flag *//*
+#if STM32 // TODO
+   //Initially clear flag
     (void) SysTick->CTRL;
     while (milliseconds != 0) {
-        *//* COUNTFLAG returns 1 if timer counted to 0 since the last flag read *//*
+        //COUNTFLAG returns 1 if timer counted to 0 since the last flag read
         milliseconds -= (SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk) >> SysTick_CTRL_COUNTFLAG_Pos;
     }
 #endif // STM32
-    return MemoryErrorHandling::MEM_NO_ERROR;*/
+    return MemoryErrorHandling::MEM_NO_ERROR;
 }
