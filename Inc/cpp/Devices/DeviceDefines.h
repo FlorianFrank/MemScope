a68ee90521b10ca5da81187f065094b5e58e21af
Florian Frank <Florian.Frank@uni-passau.de>
2022-02-18 10:12:29 +0100

    Add additional GPIO properties and states to the DeviceDefines.

--- Inc/cpp/Devices/DeviceDefines.h
+++ Inc/cpp/Devices/DeviceDefines.h
@@ -6,6 +6,7 @@
 #define MEMORY_TESTING_FW_DEVICEDEFINES_H
 #include "cpp/MemoryErrorHandling.h"
 #include <cstdint>
+#include <string>
 
 #if STM32
 #include <stm32f4xx_hal_gpio.h>
@@ -24,6 +25,7 @@ typedef GPIO_TypeDef* GPIOBank;
 #define  IO_BANK_UNDEFINED  ((GPIO_TypeDef *) nullptr)
 
 
+#define  IO_PIN_0       GPIO_PIN_0
 #define  IO_PIN_1       GPIO_PIN_1
 #define  IO_PIN_2       GPIO_PIN_2
 #define  IO_PIN_3       GPIO_PIN_3
@@ -77,12 +79,31 @@ typedef GPIO_TypeDef* GPIOBank;
 typedef int GPIOBank;
 #endif // STM32
 
+enum GPIOMode {
+    GPIO_INPUT,
+    GPIO_OUTPUT
+
+#if STM32
+    ,GPIO_OUTPUT_PUSH_PULL,
+    GPIO_OUTPUT_OPEN_DRAIN,
+    GPIO_ALTERNATE_PUSH_PULL,
+    GPIO_ALTERNATE_OPEN_DRAIN
+#endif // STM32
+};
+
+
+enum GPIOState {
+    GPIO_SET,
+    GPIO_RESET
+};
+
 class GPIOPin {
     /**
      * @brief This class represents a GPIO pin consisting of a GPIO bank and a Pin.
      */
 public:
-    GPIOPin(GPIOBank bank, uint32_t pin): m_GPIOBank(bank), m_GPIOPin(pin){ }
+    GPIOPin(GPIOBank bank, uint32_t pin, std::string description = "Not defined"): m_GPIOBank(bank),
+    m_GPIOPin(pin), m_Description(description){ }
 
     /**
      * @brief Overloaded operator allows the comparison between two different GPIOPins.
@@ -107,5 +128,6 @@ public:
 private:
     GPIOBank m_GPIOBank;
     uint32_t m_GPIOPin;
+    std::string m_Description;
 };
 #endif // MEMORY_TESTING_FW_DEVICEDEFINES_H
