184d37ee255c009b4656f418e28bfbe057f37c3c
Florian Frank <Florian.Frank@uni-passau.de>
2025-03-17 21:08:50 +0100

    Disable unnecessary commands

--- compile_fw_STM32F4.bash
+++ compile_fw_STM32F4.bash
@@ -5,13 +5,8 @@ rm -r ./bin
 
 mkdir -p ./tmp
 mkdir -p ./bin
-pushd ./tmp;
+pushd ./tmp
 cmake .. -DBoardName="stm32f429"  -DBoardClass=STM32F4 -DCPU=cortex-m4 -DFPUType=hard -DFPUSpecification=fpv4-sp-d16 -Dspecs=rdimon.specs -DOS_USE_SEMIHOSTING=1
 make
 make install
-popd;
-
-# Convert .elf to binary
-pushd ./bin
-arm-none-eabi-objcopy -O binary Memory_Testing_FW.elf Memory_Testing_FW.bin
 popd
