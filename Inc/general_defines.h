020dbe7de85670c1f3752c2a1f101836a9e87247
Florian Frank <Florian.Frank@uni-passau.de>
2025-03-19 10:04:06 +0100

    Cleanup Main.cpp and add general_definitions for main file.

---
+++ Inc/general_defines.h
@@ -0,0 +1,5 @@
+#pragma once
+
+constexpr uint32_t MAIN_LOOP_DELAY = 250U;
+constexpr const char* DEFAULT_UART_PORT = "USART1";
+constexpr uint32_t DEFAULT_BAUDRATE = 115200U;
--- Src/cpp/Main/Main.cpp
+++ Src/cpp/Main/Main.cpp
@@ -1,7 +1,10 @@
 /**
+ * @file Main.cpp
  * @author Florian Frank
- * @copyright University of Passau - Chair of computer engineering
+ * @copyright University of Passau - Chair of Computer Engineering
+ * @brief Main entry point for the STM32 memory testing application.
  */
+
 #include "cpp/Main/Main.hpp"
 #include "cpp/JSONParser.h"
 #include <cpp/Devices/STM32F429Wrapper.h>
@@ -9,22 +12,57 @@
 #include <cpp/MemoryControllers/MemoryControllerParallel.h>
 #include <cpp/MemoryModules/FRAM_Rohm_MR48V256CTAZAARL.h>
 #include "cpp/MemoryTest.h"
+#include "Logger.h"
+#include "general_defines.h"
 
-#include <string.h>
+#include <memory>
+#include <string>
 #include "stm32f4xx_hal.h"
 #include "stm32f4xx_hal_uart.h"
 
-// Global Flags and Data
 bool isRunning = true;
 bool isCallbackReceived = false;
 std::string receivedCallbackData;
 
-// Callback Function
+/**
+ * @brief Callback function triggered on UART command reception.
+ * @param data Received command data.
+ */
 void onCommandReceived(std::string &data) {
     receivedCallbackData = data;
     isCallbackReceived = true;
 }
 
+/**
+ * @brief Initializes the UART interface.
+ * @param uartWrapper Unique pointer to UARTWrapper.
+ * @return Memory error code.
+ */
+MEM_ERROR setupUARTInterface(std::unique_ptr<UARTWrapper> &uartWrapper) {
+    uartWrapper = std::make_unique<UARTWrapper>(
+            DEFAULT_UART_PORT, DEFAULT_BAUDRATE, UARTWrapper_TRANSMIT_RECEIVE,
+            UARTWrapper_WORD_LENGTH_8, UARTWrapper_NO_PARITY,
+            UARTWrapper_STOP_BITS_1, UART_MODE_INTERRUPT
+    );
+
+    auto retCode = uartWrapper->Initialize();
+    if (retCode != MemoryErrorHandling::MEM_NO_ERROR) {
+        Logger::log(LogLevel::ERROR, "UART initialization failed: %s", MemoryErrorHandling::memErrorToString(retCode));
+        return retCode;
+    }
+
+    uartWrapper->RegisterReceiveCallback(onCommandReceived);
+    retCode = uartWrapper->ReceiveData(nullptr, nullptr, NON_BLOCKING, 0);
+    if (retCode != MemoryErrorHandling::MEM_NO_ERROR) {
+        Logger::log(LogLevel::ERROR, "UART receive setup failed: %s", MemoryErrorHandling::memErrorToString(retCode));
+    }
+    return retCode;
+}
+
+/**
+ * @brief Main entry point.
+ * @return Program exit code.
+ */
 int main() {
 #if STM32
     STM32F429Wrapper device;
@@ -35,27 +73,25 @@ int main() {
     MemoryControllerParallel memoryController(nullptr, fram, device);
     memoryController.Initialize();
 
-    auto uartWrapper = std::make_unique<UARTWrapper>(
-            "USART1", 115200, UARTWrapper_TRANSMIT_RECEIVE,
-            UARTWrapper_WORD_LENGTH_8, UARTWrapper_NO_PARITY,
-            UARTWrapper_STOP_BITS_1, UART_MODE_INTERRUPT
-    );
-    uartWrapper->Initialize();
-    uartWrapper->RegisterReceiveCallback(onCommandReceived);
-    uartWrapper->ReceiveData(nullptr, nullptr, NON_BLOCKING, 0);
+    std::unique_ptr<UARTWrapper> uartWrapper;
+    auto ret = setupUARTInterface(uartWrapper);
+    if (ret != MemoryErrorHandling::MEM_NO_ERROR) {
+        Logger::log(LogLevel::INFO, "Exiting program due to UART setup failure.");
+        return 0;
+    }
 
     PUFConfiguration config;
     MemoryTest test;
 
-    // Main Loop
     while (isRunning) {
         if (isCallbackReceived) {
             parse_json(receivedCallbackData.c_str(), &config);
             isCallbackReceived = false;
-            static MemoryErrorHandling::MEM_ERROR MemoryTestFactory(MemoryTest **memoryTest, MemoryController *memoryController, TimeMeasurement *timeMeasurement);
-
+            //static MemoryErrorHandling::MEM_ERROR MemoryTestFactory(
+            //      MemoryTest **memoryTest, MemoryController *memoryController,
+            //      TimeMeasurement *timeMeasurement);
         }
 
-        HAL_Delay(250);  // 250 ms delay
+        HAL_Delay(MAIN_LOOP_DELAY);
     }
 }
