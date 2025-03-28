cfe88f18ddaf82afa7fe5c8196276ac42f1f63dc
Florian Frank <Florian.Frank@uni-passau.de>
2025-03-28 09:03:08 +0100

    Increase baudrate to 921600

--- Inc/general_defines.h
+++ Inc/general_defines.h
@@ -2,4 +2,4 @@
 
 constexpr uint32_t MAIN_LOOP_DELAY = 250U;
 constexpr const char* DEFAULT_UART_PORT = "USART1";
-constexpr uint32_t DEFAULT_BAUDRATE = 115200U;
+constexpr uint32_t DEFAULT_BAUDRATE = 921600U;
